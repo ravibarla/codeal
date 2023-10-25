aa2117655ec35e3e2f6e5ae227454d9ea8d27496
ravi barla <ravibarla1@gmail.com>
2023-10-25 22:10:51 +0530

    setting up view engine to ejs

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from "express"
 import { router } from "./routes/index.js"
 const app = express()
 const port = 3200
+app.set("view engine", "ejs")
+app.set("views", "./views")
 app.use("/", router)
 app.get("/", (req, res) => {
     return res.send("hello world")
 })
+
 app.listen(port, (req, res) => {
     console.log("app is running successfully on port :", port)
 })

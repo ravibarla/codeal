2b1ee441fc43116007441ff64eb8040fd03d6c00
ravi barla <ravibarla1@gmail.com>
2023-10-26 17:32:58 +0530

    configuring passport js

---
+++ config/passport-local-stratergy.js
@@ -0,0 +1,36 @@
+import passport from "passport"
+import { Strategy } from "passport-local"
+import { User } from "../models/users.js"
+
+//authentication using passport
+passport.use(new Strategy({
+    usernameField: "email",
+
+}, (email, password, done) => {
+    //find a user and establish an identity
+    User.findOne({ email: email })
+        .catch(err => { console.log("error in finding user-->passport"); return done(err) })
+        .then(user => {
+            if (!user || user.password != password) {
+                console.log("invalid user id or password")
+                return done(null, false)
+            }
+            return done(null, user)
+        })
+}
+))
+
+//serializing the user to keep in the cookies
+passport.serializeUser((user, done) => {
+    done(null, user._id)
+})
+//deserializing the user from the cookies
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .catch(err => {
+            console.log("error in finding user"); return
+        })
+        .then(user => {
+            return done(null, user)
+        })
+})

de6baaa15f5e5eaede0653037e6eb24b3287de26
ravi barla <ravibarla1@gmail.com>
2023-10-28 18:14:39 +0530

    updating mongodb config

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost/codeal_development")
+mongoose.connect("mongodb://127.0.0.1:27017/codeal_development")
 
 export const db = mongoose.connection
 

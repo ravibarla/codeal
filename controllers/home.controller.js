ac98d8303ec0d3c24cb748c35ec043b787d8ffd8
ravi barla <ravibarla1@gmail.com>
2023-10-29 23:46:41 +0530

    display post along with the user

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,6 +1,24 @@
+import { Post } from "../models/post.js";
+
 export const home = (req, res) => {
-    console.log(req.cookies)
-    return res.render("home", {
-        title: "home"
-    })
-}
+  // console.log(req.cookies)
+  // res.cookies("user_id",25)
+  //   Post.find({})
+  //     .then((posts) => {
+  //       return res.render("home", {
+  //         title: "codeal |home",
+  //         posts: posts,
+  //       });
+  //     })
+  //     .catch((err) => console.log("error in finding the posts"));
+
+  //populate the user for each posts
+  Post.find({})
+    .populate("user")
+    .then((posts) => {
+      return res.render("home", {
+        title: "codeal |home",
+        posts: posts,
+      });
+    });
+};

0a2fdea5934a0d614a9e87a6e984337cda70c113
ravi barla <ravibarla1@gmail.com>
2023-10-29 23:53:56 +0530

    check authentication on creating posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,11 @@
 import express from "express";
 import { create } from "../controllers/posts.controller.js";
+import passport from "passport";
 
 export const router = express.Router();
 
-router.post("/create", create);
+router.post(
+  "/create",
+  (req, res, next) => passport.checkAuthentication(req, res, next),
+  create
+);

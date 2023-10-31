7be341bf87cd1d0294e514dc35bcdc245fa7b049
ravi barla <ravibarla1@gmail.com>
2023-10-31 22:49:08 +0530

    distributing the code into partials

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,5 +1,5 @@
 import { Post } from "../models/post.js";
-
+import { User } from "../models/users.js";
 export const home = (req, res) => {
   // console.log(req.cookies)
   // res.cookies("user_id",25)

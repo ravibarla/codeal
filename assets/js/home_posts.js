70031569615f070528f935197c3bc583b2099b3e
ravi barla <ravibarla1@gmail.com>
2023-11-02 00:38:05 +0530

    creating a post :receiving data

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -9,7 +9,8 @@
         url: "/posts/create",
         data: newPostForm.serialize(),
         success: (data) => {
-          console.log(data);
+          let newpost = newPostDom(data.data.post);
+          $("#post-list-container>ul").prepend(newpost)
         },
         error: (error) => {
           console.log(error);
@@ -18,5 +19,41 @@
     });
   };
   //   method to create a post to DOM
+  let newPostDom = (post) => {
+    return $(`
+    <li id="post-${post._id}>">
+  <p>
+  
+    <small>
+      <a class="delete-post-button" href="/posts/destroy/${post.id}">X</a>
+    </small>
+    ${post.content}
+
+    <br />
+    <small>${post.user.name}</small>
+  </p>
+  <div class="post-comments">
+
+    <form action="/comments/create" method="post" id="">
+      <input
+        type="text"
+        name="content"
+        placeholder="type here to add comment"
+        required
+      />
+      <input type="hidden" name="post" value="${post._id}" />
+      <input type="submit" value="add comment" />
+    </form>
+  
+    <div class="post-comment-list">
+      <ul id="post-comments-${post._id}">
+       
+      </ul>
+    </div>
+  </div>
+</li>
+
+    `);
+  };
   createPost();
 }

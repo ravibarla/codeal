977d7c04ecd6e57a6ae175aed4f3ef1a5a267ca3
ravi barla <ravibarla1@gmail.com>
2023-11-04 14:15:39 +0530

    installing multer

--- models/users.js
+++ models/users.js
@@ -1,20 +1,26 @@
 import mongoose from "mongoose";
-const userSchema = new mongoose.Schema({
+import multer from "multer";
+import path from "path";
+const AVATAR_PATH = path.join("/uploads/users/avatar");
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     name: {
-        type: String,
-        require: true
-    }
-}, {
-    timestamps: true
-})
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const User = mongoose.model("User", userSchema)
+export const User = mongoose.model("User", userSchema);

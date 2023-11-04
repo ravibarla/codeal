591ab73cc3e3b9f4ac2ea1da769518f881391dfb
ravi barla <ravibarla1@gmail.com>
2023-11-04 14:21:23 +0530

    configuring multer

--- models/users.js
+++ models/users.js
@@ -17,10 +17,23 @@ const userSchema = new mongoose.Schema(
       type: String,
       require: true,
     },
+    avatar: {
+      type: String,
+    },
   },
   {
     timestamps: true,
   }
 );
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.resolve(), "..", AVATAR_PATH));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
 
 export const User = mongoose.model("User", userSchema);

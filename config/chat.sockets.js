70ca24bab2967b434b467595026b51ec9c75266c
ravi barla <ravibarla1@gmail.com>
2023-11-14 22:57:03 +0530

    send and receive message

--- config/chat.sockets.js
+++ config/chat.sockets.js
@@ -16,5 +16,9 @@ export const chatSockets = (socketServer) => {
       socket.join(data.chatroom);
       io.in(data.chatroom).emit("user_joined :", data);
     });
+    // CHANGE :: detect send_message and broadcast to everyone in the room
+    socket.on("send_message", function (data) {
+      io.in(data.chatroom).emit("receive_message", data);
+    });
   });
 };

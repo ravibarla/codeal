9cc45383689b6030412ca97d4a1075b1dc6ed2f6
ravi barla <ravibarla1@gmail.com>
2023-11-17 00:06:55 +0530

    fixing buggs

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 import gulp from "gulp";
-// import {GulpClient} from gulp
-// import s1 from "sass"
-import sass from "gulp-sass";
 import cssnano from "gulp-cssnano";
 import rev from "gulp-rev";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+const sass = gulpSass(dartSass);
 gulp.task("css", function () {
   console.log("minifying css.....");
-  gulp.src("./assets/sass/**/*.sass")
+  gulp
+    .src("./assets/sass/**/*.sass")
     .pipe(sass())
     .pipe(cssnano())
     .pipe(gulp.dest("./assets.css"));
 
-  return gulp.src("./assets/**/*.css")
+  return gulp
+    .src("./assets/**/*.css")
     .pipe(rev())
     .pipe(gulp.dest("./public/assets"))
     .pipe(
